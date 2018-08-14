3ebd993d74d7b6787ef0ce0bc82bbe6cfffd0276
juesoslo <je.osoriol@uniandes.edu.co>
2018-08-13 19:27:17 -0500

    Agregados los modelos al Admin

--- evento/admin.py
+++ evento/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Usuario, Evento
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Usuario)
+admin.site.register(Evento)
--- eventos/urls.py
+++ eventos/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 admin.autodiscover()
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
